21b44e5434706f48726093bae6299728991c0471
oAccel <gabrielbomfimpestana@outlook.com>
2025-05-04 13:35:18 -0300

    teste distro

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://eua4.xmxcloud.net:25624/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://github.com/oAccel/AuroraLauncher/blob/main/nebula/nebulaworkspace/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
